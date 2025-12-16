f7c74a128340217e0bec861eed86ef5cea72e531
David McGrath <github.com@davidmcgrath.com>
2025-12-16 15:48:14 -0500

    ignore linting .react-router/

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,7 +2,7 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
   {
@@ -21,4 +21,8 @@ export default defineConfig([
   },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  globalIgnores([
+    '.react-router/',
+    'build/',
+  ]),
 ]);
