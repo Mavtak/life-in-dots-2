0a7792b9e9d3c6c5f26deecd3b73c4a4e12e8608
David McGrath <github.com@davidmcgrath.com>
2025-12-16 15:34:16 -0500

    configure ESLint with Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
+import eslint from "vite-plugin-eslint";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
     reactRouter(),
     tsconfigPaths(),
+    eslint({
+      lintOnStart: true,
+    }),
   ],
 });
