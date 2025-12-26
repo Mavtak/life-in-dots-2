1cc1feff1f1d269e417587bd1454b39cc2200fc2
David McGrath <github.com@davidmcgrath.com>
2025-12-25 19:52:03 -0500

    tweak coor and sizing of GraphSegments

--- app/pages/home/GraphSegment.tsx
+++ app/pages/home/GraphSegment.tsx
@@ -8,14 +8,35 @@ const Container = styled.div<{
   align-items: center;
   justify-content: center;
 
-  --size: 14px;
+  --size: 12px;
 
   width: var(--size);
   height: var(--size);
   font-size: var(--size);
 
-  ${({data}) => data.hasPassed && `
+  ${({data}) => data.isBirthWeek && `
     font-weight: 700;
+  `}
+
+  ${({data}) => !data.isBirthWeek && `
+    font-size: calc(var(--size) * 2);
+  `}
+
+  ${({data}) => data.hasPassed && !data.isBirthWeek && `
+    color: #aaaaaa;
+
+  `}
+  
+  ${({data}) => data.hasPassed && data.isBirthWeek && `
+    color: #555555;
+  `}
+
+  ${({data}) =>  !data.hasPassed  && !data.isBirthWeek && `
+    color: #444444;
+  `}
+
+  ${({data}) =>  !data.hasPassed  && data.isBirthWeek && `
+    color: #000000
   `}
 `;
 
