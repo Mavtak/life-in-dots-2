e5f32427193a78148029fd09a8aa503086696aa9
David McGrath <github.com@davidmcgrath.com>
2025-12-29 18:57:01 -0500

    extract usePosterData hook

--- app/pages/home/Page.tsx
+++ app/pages/home/Page.tsx
@@ -1,49 +1,8 @@
-import moment from "moment";
-import useStoredState from "~/utils/useStoredState";
-import getPosterData from "./getPosterData";
 import Poster from "./Poster";
-import type PosterData from "./PosterData";
-
-const convertPosterDataFromDeserializedValue = (deserializedValue: any) => {
-  const result: PosterData = {
-    ...deserializedValue,
-    birthday: moment(deserializedValue.birthday),
-  };
-
-  return result;
-};
-
-const convertPosterDataToSerializableValue = (posterData: PosterData) => {
-  const result: any = {
-    ...posterData,
-    birthday: posterData.birthday.format(),
-  };
-
-  return result;
-};
-
-const getDefaultPosterData = () => {
-  const birthday = moment('1989-10-05');
-  const name = 'David';
-  const now = moment();
-
-  const result =  getPosterData({
-    birthday,
-    maxAge: 90,
-    now,
-    name,
-  });
-
-  return result;
-};
+import usePosterData from "./usePosterData";
 
 const Page = () => {
-  const [posterData, setPosterData] = useStoredState({
-    convertFromDeserializedValue: convertPosterDataFromDeserializedValue,
-    convertToSerializableValue: convertPosterDataToSerializableValue,
-    getDefaultValue: getDefaultPosterData,
-    key: 'poster-data',
-  });
+  const [posterData, setPosterData] = usePosterData();
 
   return (
     <main>
---
+++ app/pages/home/usePosterData.ts
@@ -0,0 +1,50 @@
+import moment from "moment";
+import useStoredState from "~/utils/useStoredState";
+import getPosterData from "./getPosterData";
+import type PosterData from "./PosterData";
+
+const convertPosterDataFromDeserializedValue = (deserializedValue: any) => {
+  const result: PosterData = {
+    ...deserializedValue,
+    birthday: moment(deserializedValue.birthday),
+  };
+
+  return result;
+};
+
+const convertPosterDataToSerializableValue = (posterData: PosterData) => {
+  const result: any = {
+    ...posterData,
+    birthday: posterData.birthday.format(),
+  };
+
+  return result;
+};
+
+const getDefaultPosterData = () => {
+  const birthday = moment('1989-10-05');
+  const name = 'David';
+  const now = moment();
+
+  const result =  getPosterData({
+    birthday,
+    maxAge: 90,
+    now,
+    name,
+  });
+
+  return result;
+};
+
+const usePosterData = () => {
+  const result =  useStoredState({
+    convertFromDeserializedValue: convertPosterDataFromDeserializedValue,
+    convertToSerializableValue: convertPosterDataToSerializableValue,
+    getDefaultValue: getDefaultPosterData,
+    key: 'poster-data',
+  });
+
+  return result;
+};
+
+export default usePosterData;
