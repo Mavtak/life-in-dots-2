8fc5e43e536d0876a3a73240135e401b51d1265b
David McGrath <github.com@davidmcgrath.com>
2025-12-30 13:29:53 -0500

    refactor, extract getDefaultPosterData function

--- app/pages/home/Page.tsx
+++ app/pages/home/Page.tsx
@@ -3,21 +3,23 @@ import { useMemo } from "react";
 import getPosterData from "./getPosterData";
 import Poster from "./Poster";
 
+const getDefaultPosterData = () => {
+  const birthday = moment('1989-10-05');
+  const name = 'David';
+  const now = moment();
+
+  const result =  getPosterData({
+    birthday,
+    maxAge: 90,
+    now,
+    name,
+  });
+
+  return result;
+};
+
 const Page = () => {
-  const posterData = useMemo(() => {
-    const birthday = moment('1989-10-05');
-    const name = 'David';
-    const now = moment();
-
-    const result =  getPosterData({
-      birthday,
-      maxAge: 90,
-      now,
-      name,
-    });
-
-    return result;
-  }, []);
+  const posterData = useMemo(() => getDefaultPosterData(), []);
 
   return (
     <main>
