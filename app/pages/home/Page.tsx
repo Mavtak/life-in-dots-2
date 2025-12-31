2645dfa56ae64ea8e8cd9fbbafe5b2c299ccac3b
David McGrath <github.com@davidmcgrath.com>
2025-12-31 15:33:14 -0500

    style main content as a stacked items

--- app/pages/home/Page.tsx
+++ app/pages/home/Page.tsx
@@ -1,20 +1,27 @@
 import { useState } from 'react';
+import styled from 'styled-components';
 import usePosterData from '~/data/usePosterData';
 import Poster from './Poster';
+
+const Container = styled.main`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
 
 const Page = () => {
   const [isSelecting, setIsSelecting] = useState<boolean>(false);
   const [posterData, setPosterData] = usePosterData();
 
   return (
-    <main>
+    <Container>
       <Poster
         isSelecting={isSelecting}
         onChangeIsSelecting={setIsSelecting}
         onUpdate={setPosterData}
         value={posterData}
       />
-    </main>
+    </Container>
   );
 };
 
