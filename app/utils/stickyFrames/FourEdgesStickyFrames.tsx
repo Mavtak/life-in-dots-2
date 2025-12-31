3b0ac25a49ac90a13f87b1e1dff9219ee8798091
David McGrath <github.com@davidmcgrath.com>
2025-12-31 10:10:31 -0500

    implement FourEdgesStickyFrames component

---
+++ app/utils/stickyFrames/FourEdgesStickyFrames.tsx
@@ -0,0 +1,79 @@
+import styled from 'styled-components';
+import StickyFrame from './StickyFrame';
+
+const BottomStickyFrame = styled(StickyFrame)`
+  grid-area: bottom;
+`;
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: auto 1fr auto;
+  grid-template-rows: auto 1fr auto;
+
+  grid-template-areas:
+    'top    top    top'
+    'left   center right'
+    'bottom bottom  bottom';
+
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+
+  pointer-events: none;
+`;
+
+const LeftFrame = styled(StickyFrame)`
+  grid-area: left;
+
+  min-height: 0;
+`;
+
+const RightFrame = styled(StickyFrame)`
+  grid-area: right;
+
+  min-height: 0;
+`;
+
+const TopStickyFrame = styled(StickyFrame)`
+  grid-area: top;
+`;
+
+type Props = {
+  isShowingDebug?: boolean,
+  name: string;
+};
+
+const FourEdgesStickyFrames = ({
+  isShowingDebug,
+  name,
+  ...otherProps
+}: Props) => {
+  return (
+    <Container {...otherProps}>
+      <TopStickyFrame
+        direction="top-to-bottom"
+        isShowingDebug={isShowingDebug}
+        name={`${name}-top`}
+      />
+      <LeftFrame
+        direction="left-to-right"
+        isShowingDebug={isShowingDebug}
+        name={`${name}-left`}
+      />
+      <RightFrame
+        direction="right-to-left"
+        isShowingDebug={isShowingDebug}
+        name={`${name}-right`}
+      />
+      <BottomStickyFrame
+        direction="bottom-to-top"
+        isShowingDebug={isShowingDebug}
+        name={`${name}-bottom`}
+      />
+    </Container>
+  );
+};
+
+export default FourEdgesStickyFrames;
