ed02f3e0aac022609db18de2baf68c7eeb7e6072
David McGrath <github.com@davidmcgrath.com>
2026-01-25 18:46:12 -0500

    add close button

--- app/developmentTools/DevelopmentBanner.tsx
+++ app/developmentTools/DevelopmentBanner.tsx
@@ -1,16 +1,44 @@
 import styled from 'styled-components';
+import LinkButton from './LinkButton';
+import useDevelopmentTools from './useDevelopmentTools';
 
 const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 8px;
+
   background-color: #4d007f;
   padding: 8px;
   color: white;
   border-bottom: 1px solid black;
+
+  :first-child {
+    flex-grow: 1;
+  }
 `;
 
 const DevelopmentBanner = () => {
+  const developmentTools = useDevelopmentTools();
+
+  const renderCloseButton = () => {
+    const handlePress = () => {
+      developmentTools.banner.setIsOpen(false);
+    };
+
+    return (
+      <LinkButton
+        label="close"
+        onPress={handlePress}
+      />
+    );
+  };
+
   return (
     <Container>
-      development banner
+      <div>
+        development banner
+      </div>
+      {renderCloseButton()}
     </Container>
   );
 };
