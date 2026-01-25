b00c9e4f5c519664d3c6ef2a88eb92358dcd1ada
David McGrath <github.com@davidmcgrath.com>
2026-01-25 18:28:05 -0500

    implement blank development banner

---
+++ app/developmentTools/DevelopmentBanner.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  background-color: #4d007f;
+  padding: 8px;
+  color: white;
+  border-bottom: 1px solid black;
+`;
+
+const DevelopmentBanner = () => {
+  return (
+    <Container>
+      development banner
+    </Container>
+  );
+};
+
+export default DevelopmentBanner;
